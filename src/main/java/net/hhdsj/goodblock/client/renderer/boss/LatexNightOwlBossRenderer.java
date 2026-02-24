1ac8e893cc99638a40a7c7981ed92a7ef0c34050
hhdsjgit <abc18688567431@163.com>
2026-02-24 14:14:16 +0800

    Added Night Owl Entity to boss

--- src/main/java/net/hhdsj/goodblock/client/renderer/boss/LatexNightOwlBossRenderer.java
+++ src/main/java/net/hhdsj/goodblock/client/renderer/boss/LatexNightOwlBossRenderer.java
@@ -1,18 +1,14 @@
 package net.hhdsj.goodblock.client.renderer.boss;
 
-import net.foxyas.changedaddon.process.features.DodgeAbilityHandle;
 import net.hhdsj.goodblock.client.model.boss.ModelLatexNightOwlBoss;
 import net.hhdsj.goodblock.entity.boss.LatexNightOwlBossEntity;
-import net.ltxprogrammer.changed.client.AbilityRenderer;
 import net.ltxprogrammer.changed.client.renderer.AdvancedHumanoidRenderer;
 import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.GasMaskLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.TransfurCapeLayer;
 import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexMaleWingedDragonModel;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.layers.EyesLayer;
 import net.minecraft.resources.ResourceLocation;
 
 public class LatexNightOwlBossRenderer extends AdvancedHumanoidRenderer<LatexNightOwlBossEntity, ModelLatexNightOwlBoss, ArmorLatexMaleWingedDragonModel<LatexNightOwlBossEntity>> {
--- src/main/java/net/hhdsj/goodblock/entity/boss/LatexNightOwlBossEntity.java
+++ src/main/java/net/hhdsj/goodblock/entity/boss/LatexNightOwlBossEntity.java
@@ -6,21 +6,29 @@ import net.ltxprogrammer.changed.entity.ChangedEntity;
 import net.ltxprogrammer.changed.entity.LatexType;
 import net.ltxprogrammer.changed.entity.TransfurMode;
 import net.ltxprogrammer.changed.init.ChangedAttributes;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerBossEvent;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.BossEvent;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.ForgeMod;
 import net.minecraftforge.network.NetworkHooks;
 import net.minecraftforge.network.PlayMessages;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
+import net.minecraft.network.chat.Component;
 
 public class LatexNightOwlBossEntity extends ChangedEntity {
 
@@ -74,10 +82,59 @@ public class LatexNightOwlBossEntity extends ChangedEntity {
         super(type, world);
         xpReward = 0;
         setNoAi(false);
+        this.bossEvent = new ServerBossEvent(
+                new TranslatableComponent("entity.goodblock.boss_heath.night_owl"), // 或者用
+                BossEvent.BossBarColor.RED,
+                BossEvent.BossBarOverlay.NOTCHED_10 // 进度条样式
+        );
+        // 默认设置为可见
+        this.bossEvent.setVisible(true);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        // 更新血条进度
+        if (!this.level.isClientSide) {
+            float healthPercent = this.getHealth() / this.getMaxHealth();
+            this.bossEvent.setProgress(healthPercent);
+        }
+    }
+
+    @Override
+    public void startSeenByPlayer(@NotNull ServerPlayer player) {
+        super.startSeenByPlayer(player);
+        this.bossEvent.addPlayer(player);
+    }
+
+    @Override
+    public void stopSeenByPlayer(@NotNull ServerPlayer player) {
+        super.stopSeenByPlayer(player);
+        this.bossEvent.removePlayer(player);
+    }
+
+    @Override
+    public void aiStep() {
+        super.aiStep();
+
+        if (this.getHealth() < this.getMaxHealth() * 0.3) {
+            this.bossEvent.setName(
+                    new TranslatableComponent("entity.goodblock.boss_heath.night_owl")
+                            .withStyle(ChatFormatting.RED)
+            );
+        }
+    }
+
+    @Override
+    public void remove(RemovalReason reason) {
+        this.bossEvent.setVisible(false);
+        super.remove(reason);
     }
 
     public static void init() {
     }
+
+    private final ServerBossEvent bossEvent;
 
     public static AttributeSupplier.Builder createAttributes() {
         AttributeSupplier.Builder builder = Mob.createMobAttributes();
